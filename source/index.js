110c72f33479631488c7ec24cdafb8543a243adf
Zhetchko Vitchev <notargetbg@gmail.com>
2018-04-12 00:30:10 +0300

    upcoming updates

--- source/index.js
+++ source/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+
 class EsportsWidget extends React.Component {
   render() {
+    console.log(this.props);
+    
     return (
       <div>
-        <h2>Esports odds widget!</h2>      
+        <h2>Esports odds widget! Boom!</h2>
+        <img src={this.props.team1Img} alt=""/>
+
+        vs
+
+        <img src={this.props.team2Img} alt=""/>
       </div>
     );
   }
