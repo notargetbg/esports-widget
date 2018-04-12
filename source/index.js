cb811b15fe08b116fed01d7906c2f2743bf78a6a
Zhetchko Vitchev <notargetbg@gmail.com>
2018-04-12 14:27:29 +0300

    Updates added - hexagon css, css/style loader

--- source/index.js
+++ source/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
+import style from './style.css';
 
 class EsportsWidget extends React.Component {
   render() {
-    console.log(this.props);
+    const {team1Img, team2Img, title, coeff1, coeff2} = this.props;
     
     return (
-      <div>
-        <h2>Esports odds widget! Boom!</h2>
-        <img src={this.props.team1Img} alt=""/>
+      <div className="widget-wrapper">
+         <h2>{title}</h2>
 
-        vs
+        <div className="hexagon-row">
+          <div className="hexagon-wrapper">
+            <div className="hexagon">
+              <div className="hexagon-inner">
+                <img className="team-image" src={team1Img} alt=""/>
+              </div>
+            </div>
+          </div>
+          <div className="hexagon-wrapper">
+            <div className="hexagon">
+              <div className="hexagon-inner">
+                <img className="team-image" src={team2Img} alt=""/>     
+              </div>     
+            </div>
+          </div>
+        </div>
 
-        <img src={this.props.team2Img} alt=""/>
+        <div className="hexagon-row">
+          <div className="hexagon-wrapper">
+            <div className="hexagon">
+              <div className="hexagon-inner">
+                <span>{coeff1}</span>
+              </div>
+            </div>
+          </div>
+          <span>
+            vs
+          </span>
+          <div className="hexagon-wrapper">
+            <div className="hexagon">
+              <div className="hexagon-inner">
+                <span>{coeff2}</span>
+              </div>     
+            </div>
+          </div>
+        </div>
+        
       </div>
     );
   }
